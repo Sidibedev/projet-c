5b130e672da54d72be73ba902e9b9d0df40377bf
Mouhamed aly sidibe <mouhamed.aly.sidibe96@gmail.com>
2017-03-21 17:03:38 +0000

    Add files via upload
    g modifier la fonction que javé mi dans le menu

--- menuprincipal.c
+++ menuprincipal.c
@@ -1,26 +1,25 @@
 
 
 // l'interface qui permet a l'utilisateur de choisir ce qu'il veut faire.
-PTR supprimer(char *mot,PTR L)
-	{
-		PTR L1,L2,L3;
-		if(L==NULL)
-			return L;
-		else
-			{
-				L1=L;
-				L2=L1->suiv;
-				while(L2!=NULL && L2->mot!=mot)
-					{
-                          L1=L2;
-						L2=L2->suiv;
-					    }
-				if(L2->mot==mot)
-					{
-						L1->suiv=L2->suiv;
+void supprimer(num , * L){
+
+
+ membre* L1 = L ;
+ bool trouve = false ;
+
+ while(L1->suivant!= NULL && trouve == false)
+    if (L2->num = num)
+    					{
+						L1->suiv=L3->suiv;
 						L2->suiv=NULL;
 						free(L2);
 					}
+         else
+             if (trouve == true )
+             L->suiv=L2->suiv;
+             L1->suiv=NULL;
+             free(L1);
+
 				return L;
-			}
-	}
+         }
+
