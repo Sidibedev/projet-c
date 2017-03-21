9be4f1c8ce150b64e0d083ef98bbd56df78ba319
Mouhamed aly sidibe <mouhamed.aly.sidibe96@gmail.com>
2017-03-21 01:24:03 +0000

    Add files via upload
    g mis la fonction supprimer dans la partie menu

---
+++ menuprincipal.c
@@ -0,0 +1,26 @@
+
+
+// l'interface qui permet a l'utilisateur de choisir ce qu'il veut faire.
+PTR supprimer(char *mot,PTR L)
+	{
+		PTR L1,L2,L3;
+		if(L==NULL)
+			return L;
+		else
+			{
+				L1=L;
+				L2=L1->suiv;
+				while(L2!=NULL && L2->mot!=mot)
+					{
+                          L1=L2;
+						L2=L2->suiv;
+					    }
+				if(L2->mot==mot)
+					{
+						L1->suiv=L2->suiv;
+						L2->suiv=NULL;
+						free(L2);
+					}
+				return L;
+			}
+	}
